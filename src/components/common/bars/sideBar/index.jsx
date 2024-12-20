99665e0c7efdbb7ec85f752f200fc0670c4aac86
bbqcream <imground11@gmail.com>
2024-12-20 10:30:09 +0900

    style :: sidebar 수정

--- src/components/common/bars/sideBar/index.jsx
+++ src/components/common/bars/sideBar/index.jsx
@@ -1,56 +1,13 @@
 import React from "react";
 import * as S from "./indexStyle";
-import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
-  const ACCESS_TOKEN = localStorage.getItem("ACCESS_TOKEN");
   const navigate = useNavigate();
-
-  const [visible, setVisible] = useState(false);
-
-  const useVisible = () => {
-    setVisible(!visible);
-  };
 
   return (
     <S.Container>
-      <Link to="/main">
-        <S.LogoImage src="/images/logo.svg" />
-      </Link>
-      <S.ProfileContainer>
-        <S.ProfileMenu style={{ display: visible ? "block" : "none" }}>
-          <S.Profile>
-            <div onClick={() => navigate("/profile")}>내 정보</div>
-            <div onClick={() => navigate("/bookmark")}>북마크</div>
-            {ACCESS_TOKEN ? (
-              <div
-                style={{ color: "#B01E1E" }}
-                onClick={() => {
-                  localStorage.removeItem("ACCESS_TOKEN");
-                  alert("로그아웃 되었습니다.");
-                  navigate("/login");
-                  window.location.reload();
-                }}
-              >
-                로그아웃
-              </div>
-            ) : (
-              <div
-                style={{ color: "#2B32B2" }}
-                onClick={() => navigate("/login")}
-              >
-                로그인
-              </div>
-            )}
-          </S.Profile>
-        </S.ProfileMenu>
-        <S.ProfileImage
-          onClick={useVisible}
-          src="/images/profile.svg"
-          alt="프로필"
-        />
-      </S.ProfileContainer>
+      <S.LogoImage src="/images/logo.svg" onClick={() => navigate("/main")} />
     </S.Container>
   );
 };
--- src/components/common/bars/sideBar/indexStyle.js
+++ src/components/common/bars/sideBar/indexStyle.js
@@ -1,59 +1,22 @@
 import styled from "styled-components";
 
-export const ProfileContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  height: 300px;
-  justify-content: space-around;
-`;
-
 export const Container = styled.div`
   min-height: 100vh;
+  height: 100vh;
   display: flex;
   flex-direction: column;
   width: 256px;
   justify-content: space-between;
   align-items: center;
   background: white;
-  border-right: 1px solid #d9d9d9;
-`;
-
-export const ProfileImage = styled.img`
-  cursor: pointer;
 `;
 
 export const LogoImage = styled.img`
-  margin-top: 1vw;
-  width: 145.14px;
-  height: 50px;
-  user-select: none;
-  -webkit-user-drag: none;
-  -khtml-user-drag: none;
-  -moz-user-drag: none;
-  -o-user-drag: none;
-`;
-
-export const ProfileMenu = styled.div`
-  position: absolute;
-  margin-bottom: 300px;
-  display: none;
-`;
-export const Profile = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 108px;
-  height: 111px;
-  border-radius: 5px;
-  width: 108px;
-  font-size: 12px;
-  box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.2);
-  justify-content: space-around;
   cursor: pointer;
+  margin-top: 1vw;
+  user-select: none;
   -webkit-user-drag: none;
   -khtml-user-drag: none;
   -moz-user-drag: none;
   -o-user-drag: none;
-  user-select: none;
 `;
