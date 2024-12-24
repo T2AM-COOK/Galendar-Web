dd9651bd1757203212ef86fe484ca7af3ac234aa
bbqcream <imground11@gmail.com>
2024-12-24 12:16:07 +0900

    fix :: 찐찐막 커밋

--- src/pages/bookMark/indexStyle.js
+++ src/pages/bookMark/indexStyle.js
@@ -3,12 +3,12 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   width: 100%;
-  height: 100vh;
+  min-height: 100vh;
 `;
 
 export const Content = styled.div`
-  width: 100%;
   margin-top: 100px;
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
